753c6cd5e526d1d5e3f78fbf0234c9a33518873d
Shivaum <shivaum1@gmail.com>
2020-04-27 18:15:47 -0700

    added folders

---
+++ ClassFiles/Class2.java
@@ -0,0 +1,82 @@
+// https://tinyurl.com/ShelterCoding
+
+public class Class2 {
+	public static void main(String[] args) {
+		// int myAge = 19; //myAge and myage are different
+		// int yourAge = 25;
+
+		// System.out.println(yourAge - myAge);
+
+		// myAge = myAge + 10; // myAge = 29
+		// System.out.println(myAge);
+		// myAge += 10; // myAge = 34
+		// System.out.println(myAge);
+
+		// System.out.println(myAge);
+
+		// int c = 5;
+		// int d = 6;
+
+
+		// c = c * d; // c = 30
+		// c *= d; // c = 180
+
+		// // %=
+		// // -=
+		// // /=
+
+		// c = 10;
+		// d = 3;
+
+		// // 10 / 3 = 3 R 1
+
+		// // // modulus
+		// System.out.println(c % d);
+
+		// c = 27;
+		// d = 4;
+		// System.out.println(c % d);
+
+		// boolean - true or false
+
+		// boolean isHappy = true;
+
+		// // isHappy = true
+		// isHappy = !isHappy;
+		// // isHappy = false
+
+		// System.out.println(isHappy);
+
+		// char letter = 'b';
+
+		String str = "This is a string. ";
+		System.out.println(str);
+
+		String newStr = "A new string.";
+		str += newStr;
+		// str = str + newStr;
+		System.out.println(str);
+
+		// // p i z z a 
+		// // 0 1 2 3 4
+
+		String myFavoriteFood = "pizza";
+		String firstThree = myFavoriteFood.substring(0, 3);
+		System.out.println(firstThree);
+
+		// A n o t h e r   s t r  i  n   g
+		// 0 1 2 3 4 5 6 7 8 9 10 11 12 13
+
+		String anotherStr = "Another string";
+
+		System.out.println(anotherStr.indexOf("str"));
+
+		System.out.println(anotherStr.length());
+
+		// String firstName = "Shivaum";
+		// String lastName = "Kumar";
+
+		// System.out.println(firstName + " " + lastName);
+
+	}
+}
